bc1bbb678f8670164305762ef0ea7088bd173d26
devAmaresh <amprasad.2003@gmail.com>
2024-12-10 07:11:14 +0530

    fixes

--- frontend/src/pages/logout/page.tsx
+++ frontend/src/pages/logout/page.tsx
@@ -1,13 +1,12 @@
 import { Spin } from "antd";
 import { useEffect } from "react";
 import Cookies from "js-cookie";
-import { useNavigate } from "react-router-dom";
+
 // logout page
 const page = () => {
-  const navigate = useNavigate();
   useEffect(() => {
     Cookies.remove("token");
-    navigate("/login");
+    window.location.href = "/login";
   }, []);
   return (
     <div className="flex justify-center items-center w-screen h-screen">
