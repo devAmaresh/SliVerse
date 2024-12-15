e8a183a4fe2846b039d1ee362eddcf2985c0abb1
devAmaresh <amprasad.2003@gmail.com>
2024-12-15 17:13:05 +0530

    fix

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-pip install -r requirements.txt  # Install dependencies
+python3.12 -m pip install --upgrade pip  # Upgrade pip
+python3.12 -m pip install -r requirements.txt  # Install dependencies
 python3.12 manage.py collectstatic --noinput  # Collect static files
