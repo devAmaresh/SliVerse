1be949e42d164606256cd49dc2513fe14ba3e868
devAmaresh <amprasad.2003@gmail.com>
2024-12-15 17:28:40 +0530

    removed vercel backend

--- backend/build_files.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-python3.12 -m pip install --upgrade pip  # Upgrade pip
-python3.12 -m pip install -r requirements.txt  # Install dependencies
-python3.12 manage.py collectstatic --noinput  # Collect static files
