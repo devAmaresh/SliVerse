7ee0db6b49c28276900e62eea1ba6d4249477b2f
devAmaresh <amprasad.2003@gmail.com>
2024-12-15 17:20:45 +0530

    done

--- backend/slides/settings.py
+++ backend/slides/settings.py
@@ -29,7 +29,7 @@ DEBUG = os.getenv("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = [".vercel.app", ".now.sh", "localhost", "127.0.0.1"]
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173","https://ai-slide-35nj.vercel.app"]
 # Application definition
 
 INSTALLED_APPS = [
