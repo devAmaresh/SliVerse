874030cd1729ef0d4031a699f24877396bfb2bcd
devAmaresh <amprasad.2003@gmail.com>
2024-12-18 20:30:32 +0530

    added loader

--- frontend/src/pages/settings/page.tsx
+++ frontend/src/pages/settings/page.tsx
@@ -3,7 +3,7 @@ import ThemeToggler from "../../components/ThemeToggler";
 import axios from "axios";
 import { backend_url } from "../../utils/backend";
 import Cookies from "js-cookie";
-import { Button, Divider, message, Modal, notification } from "antd";
+import { Button, Divider, message, Modal, notification, Skeleton } from "antd";
 import { DeleteOutlined } from "@ant-design/icons";
 import { User } from "lucide-react";
 import { useNavigate } from "react-router-dom";
@@ -11,13 +11,16 @@ const Settings = () => {
   const [user, setUser] = useState<any>(null);
   const token = Cookies.get("token");
   const [loading, setLoading] = useState(false);
+  const [fetching, setFetching] = useState(true);
   useEffect(() => {
     const fetchUser = async () => {
+      setFetching(true);
       try {
         const res = await axios.get(`${backend_url}/api/user-profile`, {
           headers: { Authorization: `Bearer ${token}` },
         });
         setUser(res.data);
+        setFetching(false);
       } catch (error) {
         console.error("Failed to fetch user:", error);
       }
@@ -70,53 +73,80 @@ const Settings = () => {
       <div className="flex justify-between items-center mb-8">
         <ThemeToggler />
       </div>
+      <div className="max-w-lg mx-auto bg-white dark:bg-zinc-800 shadow-lg rounded-xl overflow-hidden">
+        {/* Profile Section */}
+        <div className="flex flex-col items-center bg-gradient-to-r  from-indigo-400 to-blue-500 dark:from-indigo-700 dark:to-blue-600  text-white p-6">
+          {fetching ? (
+            <>
+              <Skeleton.Avatar size={90} className="mb-4" active />
+            </>
+          ) : (
+            <>
+              <img
+                src={user.profile.profile_picture}
+                alt="Profile"
+                className="w-28 h-28 rounded-full border-4 border-white shadow-lg mb-4"
+              />
+            </>
+          )}
 
-      {user ? (
-        <div className="max-w-lg mx-auto bg-white dark:bg-zinc-800 shadow-lg rounded-xl overflow-hidden">
-          {/* Profile Section */}
-          <div className="flex flex-col items-center bg-gradient-to-r  from-indigo-400 to-blue-500 dark:from-indigo-700 dark:to-blue-600  text-white p-6">
-            <img
-              src={user.profile.profile_picture}
-              alt="Profile"
-              className="w-28 h-28 rounded-full border-4 border-white shadow-lg mb-4"
-            />
-            <div className="text-sm font-medium flex items-center">
-              <User className="mr-2" />
-              {user.username}
+          <div className="text-sm font-medium flex items-center">
+            {fetching ? (
+              <Skeleton.Input style={{ width: 100 }} active />
+            ) : (
+              <>
+                <User className="mr-2" />
+                {user.username}
+              </>
+            )}
+          </div>
+        </div>
+
+        {/* Account Details */}
+        <div className="p-6 space-y-6">
+          <div>
+            <label className="block text-sm text-zinc-600 dark:text-zinc-400">
+              Name
+            </label>
+            <div className="text-lg font-semibold">
+              {fetching ? (
+                <Skeleton.Input style={{ width: 100 }} active />
+              ) : (
+                <>
+                  {user.first_name} {user.last_name}
+                </>
+              )}
             </div>
           </div>
-
-          {/* Account Details */}
-          <div className="p-6 space-y-6">
-            <div>
-              <label className="block text-sm text-zinc-600 dark:text-zinc-400">
-                Name
-              </label>
-              <div className="text-lg font-semibold">
-                {user.first_name} {user.last_name}
-              </div>
+          <div>
+            <label className="block text-sm text-zinc-600 dark:text-zinc-400">
+              Email Address
+            </label>
+            <div className="text-lg font-semibold">
+              {fetching ? (
+                <Skeleton.Input style={{ width: 100 }} active />
+              ) : (
+                user.email
+              )}
             </div>
-            <div>
-              <label className="block text-sm text-zinc-600 dark:text-zinc-400">
-                Email Address
-              </label>
-              <div className="text-lg font-semibold">{user.email}</div>
-            </div>
-            <div>
-              <label className="block text-sm text-zinc-600 dark:text-zinc-400">
-                Account Created
-              </label>
-              <div className="text-lg font-semibold">
-                {new Date(user.profile.created_at).toLocaleDateString()}
-              </div>
+          </div>
+          <div>
+            <label className="block text-sm text-zinc-600 dark:text-zinc-400">
+              Account Created
+            </label>
+            <div className="text-lg font-semibold">
+              {fetching ? (
+                <>
+                  <Skeleton.Input style={{ width: 100 }} active />
+                </>
+              ) : (
+                <>{new Date(user.profile.created_at).toLocaleDateString()}</>
+              )}
             </div>
           </div>
         </div>
-      ) : (
-        <div className="flex justify-center items-center min-h-[50vh]">
-          <p className="text-lg text-zinc-600 dark:text-zinc-400">Loading...</p>
-        </div>
-      )}
+      </div>
+
       <Divider />
       <div>
         <div className="text-red-500">Danger Zone</div>
