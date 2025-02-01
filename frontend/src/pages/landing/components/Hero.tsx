b4fc097da81173b70acd3087ea534f3c27b4c174
devAmaresh <amprasad.2003@gmail.com>
2025-02-01 16:53:57 +0530

    ui

--- frontend/src/pages/landing/components/Hero.tsx
+++ frontend/src/pages/landing/components/Hero.tsx
@@ -1,5 +1,7 @@
 import { ArrowRight, Play } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+import HeroVideoDialog from "../../../components/ui/hero-video-dialog";
+import { HashLink } from "react-router-hash-link";
 
 export function Hero() {
   const navigate = useNavigate();
@@ -34,17 +36,35 @@ export function Hero() {
             >
               Try for free <ArrowRight className="h-5 w-5" />
             </button>
-            <button className="px-4 sm:px-8 py-3 border border-gray-300 dark:border-gray-700 text-gray-700 dark:text-gray-300 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-800 transition flex items-center gap-2">
-              <Play className="h-5 w-5" /> Watch demo
-            </button>
+            <HashLink to="/#hero-video">
+              <button className="px-4 sm:px-8 py-3 border border-gray-300 dark:border-gray-700 text-gray-700 dark:text-gray-300 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-800 transition flex items-center gap-2">
+                <Play className="h-5 w-5" /> Watch demo
+              </button>
+            </HashLink>
           </div>
         </div>
-        <div className="mt-20">
-          <img
+        <div className="mt-20 flex justify-center" id="hero-video">
+          {/* <img
             src="https://images.unsplash.com/photo-1557426272-fc759fdf7a8d?auto=format&fit=crop&w=2000&q=80"
             alt="AI Presentation Dashboard"
-            className="rounded-xl shadow-2xl border border-gray-200 dark:border-gray-800"
-          />
+            className="rounded-xl shadow-2xl border border-gray-200 dark:border-gray-800 md:max-w-3xl xl:max-w-6xl xl:max-h-[40vh]"
+          /> */}
+          <div className="relative md:pt-24 pt-20">
+            <HeroVideoDialog
+              className="dark:hidden block"
+              animationStyle="top-in-bottom-out"
+              videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
+              thumbnailSrc="/hero-light.png"
+              thumbnailAlt="Hero Video"
+            />
+            <HeroVideoDialog
+              className="hidden dark:block"
+              animationStyle="top-in-bottom-out"
+              videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
+              thumbnailSrc="/hero-dark.png"
+              thumbnailAlt="Hero Video"
+            />
+          </div>
         </div>
       </div>
     </div>
